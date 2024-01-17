1301e64c6daf03244d560e5e436f531fa71fcb3c
Alaa Ahmed <lolyragab353@gmail.com>
2024-01-17 15:30:15 +0200

    first commit

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_1_Click(object sender, RoutedEventArgs e)
+        {
+            {
+               
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+          
+
+
+            
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void button__equal_Click(object sender, RoutedEventArgs e)
+        {
+            Button equal = (Button)sender;
+            {
+                if (equal != null)
+                {
+                    
+                }
+            }
+
+
+
+
+            
+            
+        }
+    }
+}
